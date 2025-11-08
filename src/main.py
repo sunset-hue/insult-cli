02e9c3f2a50f27c6f1f92a5fc99e186960450936
sunset-hue <sunsetree@proton.me>
2025-11-07 22:17:19 -0800

    fixed some bad wording

--- src/main.py
+++ src/main.py
@@ -10,7 +10,7 @@ def insult():
     a.add_argument("me", action="store_true", required=False)
     a.add_argument("-insult", action="store_true", required=False)
     a.add_argument("--path", action="store_const", required=False)
-    a.add_argument("myself", action="store_true", required=False)
+    a.add_argument("you", action="store_true", required=False)
     a.add_argument("-compliment", action="store_true", required=False)
     a.add_argument("--help", action="store_true", required=False)
     a.parse_args()
